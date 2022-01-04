d28943795485625096e98c5187b027fb8a4eb75b
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-04 15:16:56 +0100

    Negative and Positive

--- script.js
+++ script.js
@@ -16,35 +16,48 @@
 
 // Note: if there is nothing to sum, the sum is default to 0.
 
-function positiveSum(arr) {
-  let total = 0;
-  for (let i = 0; i < arr.length; i++) {
-    // setup loop to go through array of given length
-    if (arr[i] > 0) {
-      // if arr[i] is greater than zero
-      total += arr[i]; // add arr[i] to total
-    }
-  }
-  return total; // return total
-}
+// function positiveSum(arr) {
+//   let total = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     // setup loop to go through array of given length
+//     if (arr[i] > 0) {
+//       // if arr[i] is greater than zero
+//       total += arr[i]; // add arr[i] to total
+//     }
+//   }
+//   return total; // return total
+// }
 
-console.log(positiveSum([-1, -4, 7, 12]));
+// console.log(positiveSum([-1, -4, 7, 12]));
 
-console.log(positiveSum([-1, -4, 7, 12]));
+// console.log(positiveSum([-1, -4, 7, 12]));
 
-//Total pressure calculation
+// //Total pressure calculation
 
-let solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) =>
-  ((givenMass1 / molarMass1 + givenMass2 / molarMass2) *
-    0.082 *
-    (temp + 273.15)) /
-  volume;
-console.log(solution(44, 30, 3, 2, 5, 50));
-console.log(solution(60, 20, 10, 30, 10, 100));
+// let solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) =>
+//   ((givenMass1 / molarMass1 + givenMass2 / molarMass2) *
+//     0.082 *
+//     (temp + 273.15)) /
+//   volume;
+// console.log(solution(44, 30, 3, 2, 5, 50));
+// console.log(solution(60, 20, 10, 30, 10, 100));
 
-function warnTheSheep(queue) {
-  const position = queue.reverse().indexOf('wolf');
-  return position === 0
-    ? 'Pls go away and stop eating my sheep'
-    : `Oi! Sheep number ${position}! You are about to be eaten by a wolf!`;
-}
+// function warnTheSheep(queue) {
+//   const position = queue.reverse().indexOf('wolf');
+//   return position === 0
+//     ? 'Pls go away and stop eating my sheep'
+//     : `Oi! Sheep number ${position}! You are about to be eaten by a wolf!`;
+// }
+
+// ///////////////////
+// Description:
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?////////////////
+
+// function makeNegative(num) {
+//   if (num > 0) {
+//     return (num = -num);
+//   } else {
+//     return num;
+//   }
+// }
+// console.log(makeNegative(0));
